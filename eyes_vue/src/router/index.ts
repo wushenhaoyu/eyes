10e85af04a09b57d3420536f73fcf961e260cb13
wushenhaoyu <1192511920@qq.com>
2024-03-09 00:25:25 +0800

    界面init

--- eyes_vue/src/router/index.ts
+++ eyes_vue/src/router/index.ts
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import index from '../views/index.vue'
+import login from '../views/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path:'/',
+      component:login,
+      children:[
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path:'/index',
+      component:index,
+      children:[
+        
+      ]
     }
   ]
 })
---
+++ eyes_vue/src/views/index.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="common-layout">
+      <el-container>
+        <!--此处开始为header-->
+        <el-header style="padding: 0;">
+            <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      @select="handleSelect"
+      style="display: flex; justify-content: right;"
+    >
+      <el-menu-item index="1">Processing Center</el-menu-item>
+      <el-sub-menu index="2">
+        <template #title>Workspace</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-sub-menu index="2-4">
+          <template #title>item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-menu-item index="3" disabled>Info</el-menu-item>
+      <el-menu-item index="4">Orders</el-menu-item>
+    </el-menu>
+        </el-header>
+        <!--此处结束header-->
+        <el-container>
+            <!--此处开始为aside-->
+      <el-aside style="">
+        <el-row class="tac">
+      <el-col >
+       <!-- <h5 class="mb-2">Custom colors</h5>-->
+        <el-menu
+          active-text-color="#ffd04b"
+          background-color="#545c64"
+          class="el-menu-vertical-demo"
+          default-active="2"
+          text-color="#fff"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>Navigator One</span>
+            </template>
+            <el-menu-item-group title="Group One">
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title>item four</template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <el-icon><document /></el-icon>
+            <span>Navigator Three</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon><setting /></el-icon>
+            <span>Navigator Four</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
+      </el-aside>
+      <!--此处结束aside-->
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </template>
+  
+
+
+  
+
+  
+ 
+
+  <script lang="ts" setup>
+/************此处开始aside*************/
+  import {
+    Document,
+    Menu as IconMenu,
+    Location,
+    Setting,
+  } from '@element-plus/icons-vue'
+  const handleOpen = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+  }
+  const handleClose = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+  }
+/************此处结束aside*************/
+/************此处开始header*************/
+  import { ref } from 'vue'
+  
+  const activeIndex = ref('1')
+  const activeIndex2 = ref('1')
+  const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+  }
+/************此处结束header*************/
+  </script>
+   
